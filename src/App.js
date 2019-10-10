7115f9c90999cc11451d7c87bfdb76b32fb5349c
jbeltrami <joao_beltrami@hotmail.com>
2019-10-10 10:21:26 -0400

    Create Navbar component.
    Add JSX return to Navbar.
    Import that into App.js
    
    Next step: create auth links and add them to navbar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+// components
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>Marioplan</h1>
+        <Navbar />
       </div>
     </BrowserRouter>
   );
