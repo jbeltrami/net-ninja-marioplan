74a3b2cd71a525391afcb9ee6a94956d753c53ee
jbeltrami <joao_beltrami@hotmail.com>
2019-10-10 10:59:09 -0400

    Create Dashboard component and add a few fake Projects.
    Create Dashboard component, and call both ProjectList and Notifications components.
    Create a ProjectSummary to separate individual cards from the list.
    Add a Switch with a Route to handle the Dashboard link on App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // components
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// components
+import ProjectSummary from './ProjectSummary';
+
+const ProjectList = () => (
+  <div className="project-list section">
+    <ProjectSummary />
+    <ProjectSummary />
+    <ProjectSummary />
+  </div>
+);
+
+export default ProjectList;
