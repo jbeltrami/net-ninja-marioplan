548eca9ce8335a425cf8d117fb7c7df7551f46bb
jbeltrami <joao_beltrami@hotmail.com>
2019-10-10 13:44:00 -0400

    Create SignIn and SignUp components
    Create mark-up and initial logic for both components.
    Bring materializecss scripts into index.html to handle view logic.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignInAuth from './components/auth/SignInAuth';
+import SignUpAuth from './components/auth/SignUpAuth';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignInAuth} />
+          <Route path="/signup" component={SignUpAuth} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/auth/SignInAuth.js
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { Component } from 'react';
+
+export default class SignInAuth extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button type="submit" className="btn pink lighten-1 z-depth-0">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
