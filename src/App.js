effdbf798105708221b4988d2a58c6470b564ae9
jbeltrami <joao_beltrami@hotmail.com>
2019-10-10 10:11:46 -0400

    Initial commit with project starting files structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Marioplan</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
