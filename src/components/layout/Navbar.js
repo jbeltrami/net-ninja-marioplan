a3db7169bd9b350300444059a400bb58e6813dcc
jbeltrami <joao_beltrami@hotmail.com>
2019-10-10 10:44:08 -0400

    Add links to navbar.
    Create SignedInLinks and SignedOutLinks components to be displayed based on user auth.
    Add components to Navbar.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// components
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+
 const Navbar = () => (
   <nav className="nav-wrapper grey darken-3">
     <div className="container">
       <Link to="/" className="brand-logo">
         MarioPlan
       </Link>
+      <SignedInLinks />
+      <SignedOutLinks />
     </div>
   </nav>
 );
