efbfc70f2950446d6d2f265d3b16598bb6bee7a1
jbeltrami <joao_beltrami@hotmail.com>
2019-10-15 10:41:59 -0400

    Re-factor createProject function to use async/await instead of then/catch.

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,23 +1,20 @@
-export const createProject = project => (
+export const createProject = project => async (
   dispatch,
   getState,
   { getFirebase, getFirestore }
 ) => {
   // Async call to DB
-  const firestore = getFirestore();
-  firestore
-    .collection('projects')
-    .add({
-      ...project,
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorId: 12345,
-      createdAt: new Date(),
-    })
-    .then(() => {
-      dispatch({ type: 'CREATE_PROJECT', payload: project });
-    })
-    .catch(err => {
-      dispatch({ type: 'CREATE_PROJECT_ERROR', payload: err });
-    });
+  const firestore = await getFirestore();
+  await firestore.collection('projects').add({
+    ...project,
+    authorFirstName: 'Net',
+    authorLastName: 'Ninja',
+    authorId: 12345,
+    createdAt: new Date(),
+  });
+  try {
+    await dispatch({ type: 'CREATE_PROJECT', payload: project });
+  } catch (error) {
+    await dispatch({ type: 'CREATE_PROJECT_ERROR', payload: error });
+  }
 };
